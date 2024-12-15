26c7308cedc77549543ea2aef37369ce8f12343d
quannh <quan.nguyenhg12@gmail.com>
2024-12-15 13:34:56 +0700

    fix(auth): check userId in authToken exist

--- src/hooks/verifyToken.hook.ts
+++ src/hooks/verifyToken.hook.ts
@@ -2,6 +2,7 @@ import { envs } from '@configs';
 import { INVALID_TOKEN, MUST_LOGIN_FIRST } from '@constants';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import jwt from 'jsonwebtoken';
+import { prisma } from '@repositories';
 
 export async function verifyToken(req: FastifyRequest, res: FastifyReply) {
     // Get the Authorization header
@@ -17,7 +18,19 @@ export async function verifyToken(req: FastifyRequest, res: FastifyReply) {
     try {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const decodedPayload: any = jwt.verify(token, envs.JWT_SECRET);
-        req.userId = decodedPayload['userId'];
+        const userId = decodedPayload['userId'];
+
+        const user = await prisma.user.findMany({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (user.length === 0) {
+            return res.forbidden(INVALID_TOKEN);
+        }
+
+        req.userId = userId;
         return;
     } catch (err) {
         req.log.info(err);
