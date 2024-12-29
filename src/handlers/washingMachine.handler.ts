974aa5e73eaea2f529c6f21d2f1080bd3221e6c9
quannh <quan.nguyenhg12@gmail.com>
2024-12-29 18:46:05 +0700

    fix(machine): disable cancel order waiting->idle

--- src/handlers/washingMachine.handler.ts
+++ src/handlers/washingMachine.handler.ts
@@ -15,7 +15,7 @@ const fetchWashingMachines: Handler<WashingMachineDto> = async (_req, res) => {
 
         const result: WashingMachineDto = {
             idle: washingMachines.find((machine) => machine.status === 'IDLE')?._count.status || 0,
-            washing: washingMachines.find((machine) => machine.status === 'WASHING')?._count.status || 0,
+            washing: washingMachines.find((machine) => machine.status === 'WASHING' || machine.status === 'WAITING')?._count.status || 0,
             dry: washingMachines.find((machine) => machine.status === 'RINSING' || machine.status === 'SPINNING')?._count.status || 0,
         };
 
